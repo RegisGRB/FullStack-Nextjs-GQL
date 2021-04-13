91fb68b7c1eab165ab0451ba47c51f971413f5d2
GRB.github <pro.grb.studio@gmail.com>
2021-04-13 20:53:24 +0200

    Update GridCard.jsx

--- components/elements/GridCard.jsx
+++ components/elements/GridCard.jsx
@@ -7,12 +7,12 @@ const GRIDCard = styled.div`
   align-items: center;
   font-size: 3rem;
   color: #fff;
-  position:relative;
+  position: relative;
   box-shadow: rgba(3, 8, 20, 0.1) 0px 0.15rem 0.5rem,
     rgba(2, 8, 20, 0.1) 0px 0.075rem 0.175rem;
   height: 100%;
   width: 100%;
-  cursor:pointer;
+  cursor: pointer;
   border-radius: 4px;
   transition: all 500ms;
   overflow: hidden;
@@ -21,13 +21,12 @@ const GRIDCard = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-    border:1px solid ${(props) =>props.theme.colors.primary};
+  border: 1px solid ${(props) => props.theme.colors.primary};
   &:hover {
     box-shadow: rgba(2, 8, 20, 0.1) 0px 0.35em 1.175em,
       rgba(2, 8, 20, 0.08) 0px 0.175em 0.5em;
     transform: translateY(-3px) scale(1.05);
   }
-  /* Medium screens */
   @media screen and (min-width: 600px) {
     .card-tall {
       grid-row: span 2 / auto;
@@ -37,7 +36,6 @@ const GRIDCard = styled.div`
       grid-column: span 2 / auto;
     }
   }
-  /* Medium screens */
   @media screen and (min-width: 600px) {
     grid-row: ${(props) => (props.size == 1 ? "span 2 / auto" : "")};
     grid-column: ${(props) => (props.size == 3 ? "span 2 / auto" : "")};
@@ -46,4 +44,4 @@ const GRIDCard = styled.div`
   }
 `;
 
-export default GRIDCard
+export default GRIDCard;
